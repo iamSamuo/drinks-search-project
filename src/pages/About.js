049d0f2e3522d765c07521709a205a6a7b3dcf7b
iamSamuo <samuokim@gmail.com>
2022-05-12 08:06:18 +0300

    initial commit

---
+++ src/pages/About.js
@@ -0,0 +1,19 @@
+ import React from 'react'
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h2 className="section-title">about page</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim maiores, 
+        iusto delectus numquam recusandae blanditiis quisquam voluptas placeat 
+        doloribus provident vel dolores magnam at voluptate 
+        dignissimos totam vitae repellat labore,
+        expedita accusamus? Reiciendis dignissimos 
+        aliquid consequatur. Non temporibus magni delectus.
+      </p>
+    </section>
+  )
+}
+
+export default About
